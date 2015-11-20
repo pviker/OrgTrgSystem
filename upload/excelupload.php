36ded4350b033c08609d96c3545a8ab1c68a7007
pschilmoeller <pschilmoeller@me.com>
2015-11-20 08:12:15 -0600

    ..

--- upload/excelupload.php
+++ upload/excelupload.php
@@ -4,27 +4,27 @@
 //error_reporting(E_ALL ^ E_NOTICE);
 ini_set("display_errors",1);
 require_once 'excel_reader2.php';
-require_once 'db2.php';
+require_once '../controllers/db2.php';
 
 $data = new Spreadsheet_Excel_Reader("orgtest.xls");
 
-echo "Total Sheets in this xls file: ".count($data->sheets)."<br /><br />";
-
-$html="<table border='1'>";
+// echo "Total Sheets in this xls file: ".count($data->sheets)."<br /><br />";
+// 
+// $html="<table border='1'>";
 for($i=0;$i<count($data->sheets);$i++) // Loop to get all sheets in a file.
 {	
 	if(count($data->sheets[$i]['cells'])>0) // checking sheet not empty
 	{
-		echo "Sheet $i:<br /><br />Total rows in sheet $i:  ".count($data->sheets[$i]['cells'])."<br />";
+		// echo "Sheet $i:<br /><br />Total rows in sheet $i:  ".count($data->sheets[$i]['cells'])."<br />";
 		for($j=2;$j<=count($data->sheets[$i]['cells']);$j++) // loop used to get each row of the sheet
 		{ 
-			$html.="<tr>";
-			for($k=1;$k<=count($data->sheets[$i]['cells'][$j]);$k++) // This loop is created to get data in a table format.
-			{
-				$html.="<td>";
-				$html.=$data->sheets[$i]['cells'][$j][$k];
-				$html.="</td>";
-			}
+			// $html.="<tr>";
+			// for($k=1;$k<=count($data->sheets[$i]['cells'][$j]);$k++) // This loop is created to get data in a table format.
+			// {
+				// $html.="<td>";
+				// $html.=$data->sheets[$i]['cells'][$j][$k];
+				// $html.="</td>";
+			// }
             
 			$fname = mysqli_real_escape_string($connection,$data->sheets[$i]['cells'][$j][1]);
             $lname = mysqli_real_escape_string($connection,$data->sheets[$i]['cells'][$j][2]);
@@ -80,7 +80,7 @@ for($i=0;$i<count($data->sheets);$i++) // Loop to get all sheets in a file.
            
             }
             
-            $html.="</tr>";
+            // $html.="</tr>";
             
 		}
 	}
@@ -88,8 +88,8 @@ for($i=0;$i<count($data->sheets);$i++) // Loop to get all sheets in a file.
 }
 
 
-$html.="</table>";
-echo $html;
-echo "<br />Data Inserted in database";
+// $html.="</table>";
+// echo $html;
+// echo "<br />Data Inserted in database";
 //echo $data->dump(true, true);
 ?>
