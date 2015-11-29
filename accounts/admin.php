6b4afe1d7a3a27ac5a6fb9be895fc3fca5271863
Patrick Viker <pviker@me.com>
2015-11-29 12:21:32 -0600

    delete employee working
    Currently only deletes non-admins

--- accounts/admin.php
+++ accounts/admin.php
@@ -1,14 +1,34 @@
 <?php 
 
 	require("../includes/header.php");
+	
+	if(!isset($_SESSION["uname"])){
+		header("Location: ../index.php");
+	}
+	
+	if (isset($_SESSION["adminFlag"])){
+		if($_SESSION["adminFlag"] != 1){
+			header("Location: ../index.php");
+		}
+	}
+	
 	require("../includes/topmenu.php");
 	
 ?>
 
 	<div class="main-content-wrapper">
 		
+		<div><?php if(isset($_SESSION['message'])){
+						echo $_SESSION['message'];
+						unset($_SESSION['message']);
+			 		} 
+			 ?>
+		</div>
+		
 		<div>
-			<a href="manageUsers.php" class="formButton">Manage Users</a>
+			<span><a href="manageUsers.php" class="formButton">Manage Users</a></span>
+			<span><a href="manageUsers.php" class="formButton">Manage Users</a></span>
+			<span><a href="manageUsers.php" class="formButton">Manage Users</a></span>
 		</div>
 		
 	</div>
--- accounts/deleteEmployee.php
+++ accounts/deleteEmployee.php
@@ -1,6 +1,12 @@
 <?php 
 
-	require("../includes/header.php");
+	/** 
+	 * 
+	 * Dropping employee from DB is working.
+	 * 
+	 * **/
+	
+	session_start();
 
 	if(!isset($_SESSION["uname"])){
 		header("Location: ../index.php");
@@ -9,22 +15,28 @@
 	if (isset($_SESSION["adminFlag"])){
 		if($_SESSION["adminFlag"] != 1){
 			header("Location: ../index.php");
+		} 
+		else if($_SESSION['adminFlag'] == 1){
+			header("Location: admin.php");
+			$_SESSION['message'] = "Error: Cannot delete administrator";
 		}
 	}
 	
+	if(!isset($_GET['id'])){
+		header("Location: admin.php");
+	} else $employeeID = $_GET['id'];
+	
 	require("../includes/topmenu.php");
 	require("../controllers/db2.php");
+	   
+    $query1 = "delete from credentials where id = " . $employeeID;
+	$query2 = "delete from employees where id = " . $employeeID;
+    
+    if(mysqli_query($connection, $query1)){
+    	if(mysqli_query($connection, $query2)){
+    		$_SESSION['message'] = "Employee deleted successfully";
+			header("Location: admin.php");
+    	}
+    } 
 	
-?>
-
-	<div class="main-content-wrapper">DELETED</div>
-
-
-
-<!-- WILL BE USED TO DELETE USER, WAITING TO PERSIST THIS -->
-
-<?php 
-
-
-
 ?>
