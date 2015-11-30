05a0e2076884c93f46bad4d2753d77adc1312193
pschilmoeller <pschilmoeller@me.com>
2015-11-30 07:49:25 -0600

    ..

--- plans/viewPlan.php
+++ plans/viewPlan.php
@@ -2,10 +2,22 @@
 
 	require("../includes/header.php");
 	require("../includes/topmenu.php");
+    
+    
 	
 ?>
 
 <div class="main-content-wrapper">
+    
+<?php 
+    
+     if(isset($_SESSION['passConfirmMessage'])) {
+        
+        echo $_SESSION['passConfirmMessage'];
+        unset($_SESSION['passConfirmMessage']);
+     }
+    
+?>
 	
 	<h1>Current Plan</h1>
 	
